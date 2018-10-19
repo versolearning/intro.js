a9216d86a74fdd26e87dae4ecf7e0b9d98320519
Phuc Nguyen <contact@phucnguyen.info>
2018-10-19 10:24:56 +0700

    Update package info

--- intro.js
+++ intro.js
@@ -1,6 +1,6 @@
 /**
- * Intro.js v2.9.3
- * https://github.com/usablica/intro.js
+ * Intro.js v2.9.4
+ * https://github.com/versolearning/intro.js
  *
  * Copyright (C) 2017 Afshin Mehrabani (@afshinmeh)
  */
@@ -32,7 +32,7 @@
     }
 })(function () {
   //Default config/variables
-  var VERSION = '2.9.3';
+  var VERSION = '2.9.4';
 
   /**
    * IntroJs main class
